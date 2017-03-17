b60c25ebeb172d7cb3691cef500b8a437e20b970
Jak Ratiwanich <jak@triangleitc.com>
2017-03-16 23:33:21 -0700

    just ng playground project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+//import {Rx} from 'rxjs/Rx';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  s = [1,2,3,4,5];
+
+  testOb(ar: any){
+
+    Observable.from(ar).subscribe(
+      x => console.log(`onNext: ${x}`),
+      e => console.log(`onError: ${e}`),
+      () => console.log('onCompleted'));
+
+  }
 }
